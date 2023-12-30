863215d0581b7e7d1564382d3ac627944a02f792
İmranov <semtexpert@gmail.com>
2023-12-30 14:43:00 +0300

    verison 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
+
 public class Main {
+    static boolean asalSayi(int sayi, int i) {
+        if (i == 1) {
+            return true;
+        } else {
+            if (sayi % i == 0) {
+                return false;
+            } else {
+                return asalSayi(sayi, i - 1);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int sayi = inp.nextInt();
+
+        if (sayi <= 1) {
+            System.out.println(sayi + " Asal bir sayı değildir.");
+        } else {
+            if (asalSayi(sayi, sayi / 2)) {
+                System.out.println(sayi + " Asal bir sayıdır.");
+            } else {
+                System.out.println(sayi + " Asal bir sayı değildir.");
+            }
+        }
+
+
     }
 }
